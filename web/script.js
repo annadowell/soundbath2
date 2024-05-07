5de720445fbff3e9747623d4e1096340b24a9be3
Muhammad Imran <44212121+muimran@users.noreply.github.com>
2024-05-07 14:44:48 +0100

    Update script.js

--- web/script.js
+++ web/script.js
@@ -105,19 +105,19 @@ window.addEventListener("load", main);
 let geojsonData;
 
 
-
-// function mapRainfallLogarithmically(rainfall) {
-//     if (rainfall > 2) {
-//         return 7;
-//     } else if (rainfall >= 0.01) {
-//         const logBase = Math.log(2) - Math.log(0.01); // This is the scale factor
-//         const adjustedLog = Math.log(rainfall) - Math.log(0.01);
-//         return 7 * (adjustedLog / logBase);
-//     } else {
-//         return 0; // Assuming values below 0.01 should be treated as 0
-//     }
-// }
-
+// log scale starts here
+function mapRainfallLogarithmically(rainfall) {
+    if (rainfall > 2) {
+        return 7;
+    } else if (rainfall >= 0.01) {
+        const logBase = Math.log(2) - Math.log(0.01); // This is the scale factor
+        const adjustedLog = Math.log(rainfall) - Math.log(0.01);
+        return 7 * (adjustedLog / logBase);
+    } else {
+        return 0; // Assuming values below 0.01 should be treated as 0
+    }
+}
+// log scale ends here
 
 function updateAverageRainfall() {
     if (!geojsonData || !device) return;
